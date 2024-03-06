3f7d77d2f5f4c77247d23cff29201396de68c976
ndelanhese <nnathanh3@gmail.com>
2024-03-06 08:57:41 -0300

    refactor(auth-guard): change guard implementation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 @Controller("/questions")
-@UseGuards(AuthGuard("jwt"))
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
 	@Post()
 	async handle() {
